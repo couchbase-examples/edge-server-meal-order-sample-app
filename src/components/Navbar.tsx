00283bec90a8ae2f30aafb4eff0c440815b83318
AayushTyagi1 <tyagiaayush5@gmail.com>
2025-01-05 02:27:19 +0530

    Fixed NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { FaUtensils } from 'react-icons/fa';
+import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="flex items-center px-4 py-2 bg-cb-red text-white">
       <div className="flex items-center">
-        <FaUtensils size={24} className="mr-2" />
-        <span className="font-bold text-lg">Aircraft Catering</span>
+        <FlightTakeoffIcon className="mr-2" />
+        <span className="font-bold text-lg">American Airlines</span>
       </div>
       <ul className="ml-auto flex space-x-4">
         <li>Home</li>
-        <li>Menu</li>
         <li>Profile</li>
       </ul>
     </nav>
