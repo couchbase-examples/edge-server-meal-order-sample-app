2160fdd494bcc05e37692a714503e5d3aab15087
AayushTyagi1 <tyagiaayush5@gmail.com>
2025-01-07 18:01:30 +0530

    Configure localhost 6000 proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/american234.AmericanAirlines.AA234': {
+        target: 'https://localhost:60000',
+        secure: false, // Since you're using localhost with HTTPS
+        changeOrigin: true,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        configure: (proxy, _options) => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+        }
+      }
+    }
+  }
 })
