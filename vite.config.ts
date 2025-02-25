2b41dc69fc0df571e45bc2e8a5ca651ac1a0a4d1
AayushTyagi1 <tyagiaayush5@gmail.com>
2025-02-25 16:34:12 +0530

    Docker container build test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,9 +8,10 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
+      host: '0.0.0.0',
       proxy: {
         '/american234.AmericanAirlines.AA234': {
-          target: env.EDGE_SERVER_BASE_URL, // use the loaded env here
+          target: env.EDGE_SERVER_BASE_URL || 'http://localhost:60000',
           secure: false,
           changeOrigin: true,
           ws: true,
